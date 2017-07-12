2a11c7286181cdf6f6144df8caad98a5dd72b2a0
cazyw <kazeisc@gmail.com>
2017-07-13 09:31:25 +1000

    fix images and offcanvas menu

--- portfolio.js
+++ portfolio.js
@@ -2,5 +2,7 @@ $(document).ready(function () {
   $('[data-toggle="offcanvas"]').click(function () {
     $('.row-offcanvas').toggleClass('active')
   });
-
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
 });
