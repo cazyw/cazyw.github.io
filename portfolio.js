c3a56db2c49ff690bc35dfb3e32f256f3a38a5f7
cazyw <kazeisc@gmail.com>
2017-07-13 23:42:29 +1000

    fix smooth scrolling to only internal anchors

--- portfolio.js
+++ portfolio.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
   $(".navbar-nav li a").click(function(event) {
     $(".navbar-collapse").collapse('hide');
   });
-  $('a[href*=\\#]').on('click', function(event){     
+  $('a[href^=\\#]').on('click', function(event){     
     event.preventDefault();
     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
   });
