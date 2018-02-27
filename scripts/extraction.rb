c996aaf90827e04ae551b541db9a60d338c9eb2a
Caz <cazyw@users.noreply.github.com>
2018-02-27 21:55:41 +1100

    Extract project tile details into projects.json

--- scripts/extraction.rb
+++ scripts/extraction.rb
@@ -1,41 +1,9 @@
 #!/usr/bin/ruby
+require 'pp'
+require 'json'
+
 puts "=" * 20
 puts "Start ruby script"
-puts "=" * 20
-
-projects = []
-
-
-
-# <div class="col-lg-4 col-sm-6 col-xs-12">
-#           <div class="project-box" id="jsspotify">
-#             <span id="jsspotify-anchor" class="project-anchor"></span>
-#             <div class="project-image">
-#               <img src="img/js-spotify.jpg" class="img-responsive"  alt="jsspotify">
-#             </div>
-#             <div class="project-box-description">
-#               <div class="project-box-text">
-#                 <div class="project-name">
-#                   Spotify Playlist Backup
-#                 </div>
-#                 <div class="project-description">
-#                   Download track details from your playlists
-#                 </div>
-#                 <div class="project-icons">
-#                   <a href="#details" class="project-details">
-#                     <i class="fa fa-commenting-o" aria-hidden="true"></i>
-#                   </a> 
-#                   <a href="https://github.com/cazyw/spotify-playlist-backup" target="_blank" alt="https://github.com/cazyw/spotify-playlist-backup">
-#                     <i class="fa fa-github" aria-hidden="true"></i>
-#                   </a> 
-#                   <a href="https://spotify-playlist-backup.herokuapp.com/" target="_blank" alt="https://spotify-playlist-backup.herokuapp.com/">
-#                     <i class="fa fa-television" aria-hidden="true"></i>
-#                   </a>
-#                 </div>
-#               </div>
-#             </div>
-#           </div>
-#         </div>
 
 proj_id = /<div class="project-box" id="(?<project_id>\w+)">/
 proj_anchor = /<span id="(?<project_anchor>.+)" class="project-anchor">/
@@ -46,26 +14,45 @@ proj_github = /<i class="fa fa-github" aria-hidden="true">/
 proj_html = /<a href="(?<project_html>.+?)"/
 proj_demo = /<i class="fa fa-television" aria-hidden="true">/
 
+projects = []
+project = {}
+
 File.open("../index.html") do |file|
   lines = file.readlines
   lines.each_with_index do |line, index|
     if (pid = line.match(proj_id))
-      puts "Project id: #{pid['project_id']}"
+      # puts "Project id: #{pid['project_id']}"
+      project['id'] = pid['project_id']
     elsif (pid = line.match(proj_anchor))
-      puts "\tProject anchor: #{pid['project_anchor']}"
+      # puts "\tProject anchor: #{pid['project_anchor']}"
+      project['anchor'] = pid['project_anchor']
     elsif (pid = line.match(proj_img))
-      puts "\tProject image: #{pid['project_img']}"
+      # puts "\tProject image: #{pid['project_img']}"
+      project['img'] = pid['project_img']
     elsif (pid = line.match(proj_title))
-      puts "\tProject title: #{lines[index+1].strip}"
+      # puts "\tProject title: #{lines[index+1].strip}"
+      project['title'] = lines[index+1].strip
     elsif (pid = line.match(proj_description))
-      puts "\tProject description: #{lines[index+1].strip}"
+      # puts "\tProject description: #{lines[index+1].strip}"
+      project['description'] = lines[index+1].strip
     elsif (pid = line.match(proj_github))
       pid = lines[index-1].match(proj_html)
-      puts "\tProject Github link: #{pid['project_html']}"
+      # puts "\tProject Github link: #{pid['project_html']}"
+      project['github'] = pid['project_html']
     elsif (pid = line.match(proj_demo))
       pid = lines[index-1].match(proj_html)
-      puts "\tProject Demo website: #{pid['project_html']}"
+      # puts "\tProject Demo website: #{pid['project_html']}"
+      project['demo'] = pid['project_html']
+      # puts project
+      projects.push(project)
+      project = {}
     end
   end
 
+  File.open('./projects.json', 'w+') do |file|
+    file.write(JSON.pretty_generate(projects))
+  end
 end
+
+puts "JSON file generated"
+puts "=" * 20
