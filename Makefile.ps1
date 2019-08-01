8df4ee0035f6bbc81a6fd780ae7b11e3d75976bf
cazyw <8996597+cazyw@users.noreply.github.com>
2019-08-01 23:24:13 +1000

    Update makefile as exposing the port is necessary for gatsby to work locally

--- Makefile.ps1
+++ Makefile.ps1
@@ -1,6 +1,6 @@
 function Up {
   docker build . -t docker-node-image
-  docker run --rm -it -v ${PWD}:/app --name docker-node docker-node-image sh
+  docker run --rm -it -v ${PWD}:/app -p 8000:8000 --name docker-node docker-node-image sh
 }
 
 function Down {
