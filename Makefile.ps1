b32cdda6c74c2e9e884a1864f7be3d0e0c498e51
cazyw <8996597+cazyw@users.noreply.github.com>
2019-08-02 00:10:02 +1000

    update docker command so gatsby has HMR in docker

--- Makefile.ps1
+++ Makefile.ps1
@@ -1,6 +1,6 @@
 function Up {
-  docker build . -t docker-node-image
-  docker run --rm -it -v ${PWD}:/app -p 8000:8000 -e GATSBY_WEBPACK_PUBLICPATH=/ --name docker-node docker-node-10 sh
+  docker build . -t docker-node-10
+  docker run --rm -it -v ${PWD}:/app -p 8000:8000 -e GATSBY_WEBPACK_PUBLICPATH=/ -e CHOKIDAR_USEPOLLING=1 --name docker-node docker-node-10 sh
 }
 
 function Down {
