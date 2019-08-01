8e4085c6da9fcedbaa1b7e436bec07a212ce5850
cazyw <8996597+cazyw@users.noreply.github.com>
2019-08-02 00:02:06 +1000

    Update makefile with environment variable

--- Makefile.ps1
+++ Makefile.ps1
@@ -1,6 +1,6 @@
 function Up {
   docker build . -t docker-node-image
-  docker run --rm -it -v ${PWD}:/app -p 8000:8000 --name docker-node docker-node-image sh
+  docker run --rm -it -v ${PWD}:/app -p 8000:8000 -e GATSBY_WEBPACK_PUBLICPATH=/ --name docker-node docker-node-10 sh
 }
 
 function Down {
