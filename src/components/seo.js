08bfe34f57fc0e6062304e4a80b192afd5fb5aad
cazyw <8996597+cazyw@users.noreply.github.com>
2019-08-16 23:30:54 +1000

    change og:image form webp to jpg

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-import projectImage from "../images/projects.webp"
+import projectImage from "../images/projects.jpg"
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
