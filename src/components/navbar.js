7d7b8136d1f02361f99b0b9780c83e0056ace946
cazyw <8996597+cazyw@users.noreply.github.com>
2019-08-27 00:20:37 +1000

    WIP - nav bar toggle

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,22 +2,35 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+const toggleMenu = () => {
+  const navs = document.querySelector(".nav-links")
+  navs.classList.toggle("open")
+}
+
 const Navbar = ({ menuLinks }) => (
   <div>
     <nav>
-      <ul style={{ display: "flex", flex: 1 }}>
-        {menuLinks.map(link => (
-          <li
-            key={link.name}
-            style={{
-              listStyleType: `none`,
-              padding: `1rem`,
-            }}
-          >
-            <Link to={link.link}>{link.name}</Link>
-          </li>
-        ))}
-      </ul>
+      <div className="nav-brand">Home</div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className="hamburger-lines"></div>
+        <div className="hamburger-lines"></div>
+        <div className="hamburger-lines"></div>
+      </div>
+      <div className="nav-links">
+        <ul>
+          {menuLinks.map(link => (
+            <li
+              key={link.name}
+              style={{
+                listStyleType: `none`,
+                padding: `1rem`,
+              }}
+            >
+              <Link to={link.link}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   </div>
 )
